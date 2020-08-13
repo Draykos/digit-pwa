98f68e2343c8524979d08fa8a5ce26fe39d3ba81
Gianalfonso Gardini <gianalfonso@digitware.it>
2020-08-13 12:59:21 +0200

    new version

---
+++ precache-manifest.b43e463f741732245c314db5ed3ff738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41001a5598e8721d20067f248a145a79",
+    "url": "/digit-pwa/index.html"
+  },
+  {
+    "revision": "aa5f812615c1aada40b3",
+    "url": "/digit-pwa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5eda19d93ef0a09886a",
+    "url": "/digit-pwa/static/js/2.cec43390.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digit-pwa/static/js/2.cec43390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5f812615c1aada40b3",
+    "url": "/digit-pwa/static/js/main.60d1de8e.chunk.js"
+  },
+  {
+    "revision": "cc3e075a2d78b2bca08c",
+    "url": "/digit-pwa/static/js/runtime-main.28c84610.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/digit-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a68e072d6929b1d8f4df1d10895231ac.js"
+  "/digit-pwa/precache-manifest.b43e463f741732245c314db5ed3ff738.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/digit-pwa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
